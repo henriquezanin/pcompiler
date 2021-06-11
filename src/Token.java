5a010d6736ccf5f093f8d2dedf1f79364aa28a33
Gabriel Marin <gabrielvbmarin21@gmail.com>
2021-06-11 02:17:31 -0300

    Revert "remove files for merge"
    This reverts commit 252261444eedcc543e0c354c7b575ee5d84810a2.

---
+++ src/Token.java
@@ -0,0 +1,21 @@
+public class Token {
+
+    String value;
+    boolean isValid;
+
+    public String getValue() {
+        return value;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void setValid(boolean valid) {
+        isValid = valid;
+    }
+}
